277aa0b50386c17c7f8418819776f2bd63a2dbd7
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-12 14:43:22 +0700

    Create userRoutes.js

---
+++ routes/userRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/userController");
+const { StatusCodes } = require("http-status-codes");
+
+// Routing with controller methods for different HTTP methods coming into the users route
+router
+  .route("/")
+  .get(getAllUsers)
+  .post(createUser)
+  .patch(updateUser)
+  .delete(deleteUser);
+
+module.exports = router;
