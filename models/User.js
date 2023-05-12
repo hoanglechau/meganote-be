ad71655d208aeee458ef4386457ba397531e9ca2
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-12 13:21:31 +0700

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  roles: [
+    {
+      type: String,
+      default: "Employee",
+    },
+  ],
+  active: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+// Name the model "User", and pass in the user schema
+module.exports = mongoose.model("User", userSchema);
