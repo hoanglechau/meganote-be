6ead92065243aa6aa1651dd98083efc347f34c05
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-12 13:21:24 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+require("dotenv").config(); // Prefer to put at the start
 const express = require("express");
 const app = express();
 const path = require("path");
-const { logger } = require("./middleware/logger");
+const { logger, logEvents } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConnect");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB database
+connectDB();
 
 // Custom logger middleware
 app.use(logger);
@@ -42,4 +48,17 @@ app.all("*", (req, res) => {
 // Custom middleware to handle errors
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+// Connect to MongoDB and start the server
+mongoose.connection.once("open", () => {
+  console.log("⚡Connected to MongoDB");
+  app.listen(PORT, () => console.log(`⚡Server is running on port ${PORT}`));
+});
+
+// Handle MongoDB connection errors
+mongoose.connection.on("error", err => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
+});
