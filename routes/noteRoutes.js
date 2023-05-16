05101868deba41d17b0341fda50767fd8e2af6a6
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-16 11:52:15 +0700

    Create noteRoutes.js

---
+++ routes/noteRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllNotes,
+  createNote,
+  updateNote,
+  deleteNote,
+} = require("../controllers/noteController");
+
+router
+  .route("/")
+  .get(getAllNotes)
+  .post(createNote)
+  .patch(updateNote)
+  .delete(deleteNote);
+
+module.exports = router;
