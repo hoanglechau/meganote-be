8c1bfb0b57c1bef17c7ee2ff9c3d634ca4513a35
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-17 15:47:59 +0700

    Update noteRoutes.js

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -6,7 +6,12 @@ const {
   updateNote,
   deleteNote,
 } = require("../controllers/noteController");
+const verifyJWT = require("../middleware/verifyJWT");
 
+// Use the verifyJWT middleware for all routes in this file
+router.use(verifyJWT);
+
+// Routing with controller methods for different HTTP methods coming into the notes route
 router
   .route("/")
   .get(getAllNotes)
