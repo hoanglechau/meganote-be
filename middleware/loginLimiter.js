01f043f21a60e840fa38e318f509e875621e82db
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-21 22:44:55 +0700

    Update loginLimiter.js

--- middleware/loginLimiter.js
+++ middleware/loginLimiter.js
@@ -8,6 +8,7 @@ const loginLimiter = rateLimit({
     message:
       "Too many login attempts from this IP, please try again after a 60 second pause",
   },
+  // The handler will handle what will happen when the login limit is reached
   handler: (req, res, next, options) => {
     logEvents(
       `Too Many Requests: ${options.message.message}\t${req.method}\t${req.url}\t${req.headers.origin}`,
@@ -15,6 +16,7 @@ const loginLimiter = rateLimit({
     );
     res.status(options.statusCode).send(options.message);
   },
+  // These headers are recommended in the official documentation
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
