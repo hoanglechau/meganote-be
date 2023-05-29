ad6a284551e4a3f40b65b80485669eb3a183b7de
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-29 11:50:14 +0700

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,4 @@
 // TODO: Replace with deployed API URL later
-const allowedOrigins = ["http://localhost:5000"];
+const allowedOrigins = ["http://localhost:5000", "http://localhost:3000"];
 
 module.exports = allowedOrigins;
