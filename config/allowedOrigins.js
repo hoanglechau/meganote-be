39d61e7430c3b4fa53e5108eebb8cce8af318a94
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-31 14:25:56 +0700

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,3 @@
-// TODO: Replace with deployed API URL later
-const allowedOrigins = ["http://localhost:5000", "http://localhost:3000"];
+const allowedOrigins = ["http://localhost:5000", "https://meganote-be-production.up.railway.app/"];
 
 module.exports = allowedOrigins;
