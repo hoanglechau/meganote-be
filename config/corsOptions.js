a025e336eb1424323a002a5a616a59373438bf1c
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-31 16:03:43 +0700

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,13 +1,19 @@
-// const allowedOrigins = require("./allowedOrigins");
+const allowedOrigins = require("./allowedOrigins");
 
 // These options follow the rules of the CORS 3rd party middleware
 const corsOptions = {
   // Configures the Access-Control-Allow-Origin CORS header
-  origin: "*",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  preflightContinue: false,
+  origin: (origin, callback) => {
+    // Allow testing apps like Postman to be able to access this API
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      // null means no error
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   // Set the Access-Control-Allow-Credentials header
-  credentials: false,
+  credentials: true,
   // The default status code is 204, but 200 can prevent problems on some devices
   optionsSuccessStatus: 200,
 };
