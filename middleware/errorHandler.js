a69a3a88539bd088e1244118ac63b0445659bc82
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-06-28 10:12:43 +0700

    Create errorHandler.js

---
+++ middleware/errorHandler.js
@@ -0,0 +1,20 @@
+const { logEvents } = require("./logger");
+
+const errorHandler = (err, req, res, next) => {
+  logEvents(
+    `${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`,
+    "errLog.log"
+  );
+  // Log out many details of the error
+  console.log(err.stack);
+
+  const status = res.statusCode ? res.statusCode : 500; // server error
+
+  res.status(status);
+
+  // The 'isError' flag is used to determine whether the error message should be displayed to the user. This is included so that RTK Query can find it on the frontend. This is for handling errors in the apiSlice on the frontend code
+  // This is for unexpected errors (no clear error status)
+  res.json({ message: err.message, isError: true });
+};
+
+module.exports = errorHandler;
