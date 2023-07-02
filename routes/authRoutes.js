9c9015f6daa3d00a1c3cb745badd8cd4866820a1
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-03 00:31:14 +0700

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("../controllers/authController");
+const {
+  login,
+  register,
+  forgotPassword,
+  resetPassword,
+} = require("../controllers/authController");
 const loginLimiter = require("../middleware/loginLimiter");
 
-router.route("/").post(loginLimiter, authController.login);
+/**
+ * @description This file contains the routes for the auth endpoints
+ * @author [Hoang Le Chau](https://github.com/hoanglechau)
+ */
 
-router.route("/register").post(authController.register);
+router.route("/").post(loginLimiter, login);
+
+router.route("/register").post(register);
+
+router.route("/forgotpassword").post(forgotPassword);
+
+router.route("/resetpassword/:passwordResetToken").patch(resetPassword);
 
 module.exports = router;
