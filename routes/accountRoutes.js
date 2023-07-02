ae2112a772edf7e693bd8cd84df86b20e47b7eaf
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-03 00:31:12 +0700

    Update accountRoutes.js

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -3,13 +3,23 @@ const router = express.Router();
 const {
   getSingleAccount,
   updateAccount,
+  updateProfile,
 } = require("../controllers/accountController");
 const verifyJWT = require("../middleware/verifyJWT");
+
+/**
+ * @description This file contains the routes for the account endpoints
+ * @author [Hoang Le Chau](https://github.com/hoanglechau)
+ */
 
 // Use the verifyJWT middleware for all routes in this file
 router.use(verifyJWT);
 
 // Routing with controller methods for different HTTP methods coming into the users route
-router.route("/:id").get(getSingleAccount).patch(updateAccount);
+router
+  .route("/:id")
+  .get(getSingleAccount)
+  .patch(updateAccount)
+  .put(updateProfile);
 
 module.exports = router;
