b2b1cfbed5e1a0dc993bd30eeb3c7acb32409616
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-03 00:31:16 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
+/**
+ * @description This file contains the routes for the root of the application
+ * @author [Hoang Le Chau](https://github.com/hoanglechau)
+ */
+
 router.get("^/$|/index(.html)?", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "views", "index.html"));
 });
