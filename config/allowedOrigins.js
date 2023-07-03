ba39448756d5d6593b6daf615c36b6299eb0cd31
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-03 13:16:42 +0700

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,7 @@
  * @author [Hoang Le Chau](https://github.com/hoanglechau)
  */
 const allowedOrigins = [
-  "https://meganote-fe-v3.vercel.app",
+  "https://meganote-app.netlify.app/",
   "http://localhost:3000",
 ];
 
