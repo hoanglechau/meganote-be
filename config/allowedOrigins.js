34bf9b8fd7956753092136539f46d798b4561edc
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-27 15:39:54 +0700

    Update the allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,9 +2,6 @@
  * @description This file contains the whitelist of allowed origins (website domains) for CORS
  * @author [Hoang Le Chau](https://github.com/hoanglechau)
  */
-const allowedOrigins = [
-  "https://meganote.vercel.app/",
-  "http://localhost:3000",
-];
+const allowedOrigins = ["https://meganote.vercel.app", "http://localhost:3000"];
 
 module.exports = allowedOrigins;
